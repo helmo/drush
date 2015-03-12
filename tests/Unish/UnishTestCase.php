d54f22ee0ad59d0228827b7e83eba403a3b2da85
Herman van Rink <rink@initfour.nl>
2015-03-12 16:25:22 +0100

    Extend setUpDrupal() to have more control over subdir names

--- tests/Unish/UnishTestCase.php
+++ tests/Unish/UnishTestCase.php
@@ -226,9 +226,11 @@ abstract class UnishTestCase extends \PHPUnit_Framework_TestCase {
     return parse_url(UNISH_DB_URL, PHP_URL_SCHEME);
   }
 
-  function setUpDrupal($num_sites = 1, $install = FALSE, $version_string = UNISH_DRUPAL_MAJOR_VERSION, $profile = NULL) {
+  function setUpDrupal($num_sites = 1, $install = FALSE, $version_string = UNISH_DRUPAL_MAJOR_VERSION, $profile = NULL, $sites_subdirs = array()) {
     $sites_subdirs_all = array('dev', 'stage', 'prod', 'retired', 'elderly', 'dead', 'dust');
-    $sites_subdirs = array_slice($sites_subdirs_all, 0, $num_sites);
+    if (empty($sites_subdirs)) {
+      $sites_subdirs = array_slice($sites_subdirs_all, 0, $num_sites);
+    }
     $root = $this->webroot();
     $major_version = substr($version_string, 0, 1);
 
